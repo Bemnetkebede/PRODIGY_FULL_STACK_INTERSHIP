4f79ead3d81fc355739c52fc4a1775c349446867
Bemnet Kebede <bemnet026@gmail.com>
2024-11-29 16:26:16 +0300

    initial commit

---
+++ PRODIGY_FS_02/back-end/app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const mysql = require('mysql2')
+
+const app = express();
+
+app.listen(5000,()=>{console.log('server is running ')})
--- front-end/src/pages/AdminPage.jsx
+++
@@ -1,9 +0,0 @@
-const AdminPage = () => {
-    return (
-        <div className=' text-[#09585B]  text-3xl grid place-items-center font-bold mt-64'>
-            Welcome to Admin Dashboard
-        </div>
-    )
-}
-
-export default AdminPage
